e12321c3e392bb0feab10f8919ce5a04b935c57a
Alexey <alexey.simonov@gmail.com>
2018-08-23 14:09:05 +0100

    trying to make p2p udp exchange work

--- udp_client.py
+++ udp_client.py
@@ -8,7 +8,7 @@ import random
 logger = logging.getLogger()
 
 
-def main(host='127.0.0.1', port=9999):
+def main(host='127.0.0.1', port=9999, role='S'):
     sock = socket.socket(socket.AF_INET, # Internet
                          socket.SOCK_DGRAM) # UDP
     logger.info("connecting to p2p broker %s:%s", host, port)
@@ -22,15 +22,18 @@ def main(host='127.0.0.1', port=9999):
 
     msg = 0
     while True:
-        s = str(msg)
-        bs = bytes(s)
-        logger.info('sending to %s data: %s', peer_addr, bs)
-        sock.sendto(bs, peer_addr)
-        msg += 1
-        data, addr = sock.recvfrom(1024)
-        logger.info('received from addr %s data: %s', addr, data)
+        if role=='S':
+            s = str(msg)
+            bs = bytes(s)
+            logger.info('sending to %s data: %s', peer_addr, bs)
+            sock.sendto(bs, peer_addr)
+            msg += 1
+        else:
+            data, addr = sock.recvfrom(1024)
+            logger.info('received from addr %s data: %s', addr, data)
+
         time.sleep(random.randrange(0,1500) / 1000)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
-    main(*addr_from_args(sys.argv))
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
